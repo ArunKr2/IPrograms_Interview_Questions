40a9e21875f0d5b3405102ecbf12b65708b52791
Arun <you@example.com>
2026-02-25 17:19:17 +0530

    Interview Questions

---
+++ src/String_Programs/StringReverse.java
@@ -0,0 +1,19 @@
+package String_Programs;
+
+import java.util.Scanner;
+
+public class StringReverse {
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String nstr = "";
+        for(int i=str.length()-1;i>=0;i--){
+            nstr+=str.charAt(i);
+        }
+
+        System.out.println(nstr);
+    }
+}
